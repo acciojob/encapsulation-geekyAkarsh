e8c28bc58e85fdc32b95ba92235ee0ee5591be4e
Akarsh <akarshsingh81@gmail.com>
2023-04-11 05:29:48 +0530

    tried to access private member and got error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         RWOnly obj = new RWOnly();
-
+        obj.name = "new name";
+        System.out.println(obj.name);
+        // error - java: name has private access in com.driver.RWOnly
     }
 }
