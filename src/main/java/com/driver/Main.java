b2235a6c4b1719e9b2ab1366b3e35f837a776136
Akarsh <akarshsingh81@gmail.com>
2023-04-12 04:33:45 +0530

    changed the name of object

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,12 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
 
-        RWOnly obj = new RWOnly();
+        RWOnly rwOnly = new RWOnly();
 
-        // setting name value using setter -
-        obj.setName("Geek");
+        //System.out.println(rwOnly.name);
 
-        // getting name value using getter -
-        System.out.println(obj.getName());
-        // output - Geek
+        rwOnly.setName("Geek");
+
+        System.out.println(rwOnly.getName());
     }
 }
